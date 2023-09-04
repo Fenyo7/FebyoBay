85036d564b953e5c7fa1229a246ea45b5683a0fd
Fenyo7 <fenyo.lev@gmail.com>
2023-09-04 21:39:21 +0200

    fix(be): user model fix

--- Backend/Models/User.cs
+++ Backend/Models/User.cs
@@ -1,10 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace greenBayAPI.Models
 {
     public class User
     {
         public int Id { get; set; }
+        
+        [Required]
         public required string Username { get; set; }
-        public required string Password { get; set; }        
+        
+        [Required]
+        public required string Password { get; set; }
+        
+        [Required]
         public required string Email { get; set; }
     }
 }
