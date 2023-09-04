590f523482581723cf8c3233214444ec8eef1dad
Fenyo7 <fenyo.lev@gmail.com>
2023-09-04 21:25:07 +0200

    fix(be): added required to fields in database

--- Backend/Models/User.cs
+++ Backend/Models/User.cs
@@ -3,8 +3,8 @@ namespace greenBayAPI.Models
     public class User
     {
         public int Id { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }        
-        public string Email { get; set; }
+        public required string Username { get; set; }
+        public required string Password { get; set; }        
+        public required string Email { get; set; }
     }
 }
