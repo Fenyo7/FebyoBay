e224f6a72d95bf573c545bf91f99f30077f52314
Fenyo7 <fenyo.lev@gmail.com>
2023-09-04 21:24:06 +0200

    database setup and user database

---
+++ Backend/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+using greenBayAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace greenBayAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Backend/Models/User.cs
@@ -0,0 +1,10 @@
+namespace greenBayAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }        
+        public string Email { get; set; }
+    }
+}
