b043ca946ac027a21e30d7c3effb218765fa2426
Fenyo7 <fenyo.lev@gmail.com>
2023-09-19 13:26:24 +0200

    feat(fe): balance shows up on frontend

--- Frontend/src/app/components/header/header.component.ts
+++ Frontend/src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-header',
@@ -6,12 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit{
+  private subscription: Subscription = new Subscription();
   protected username: string | null = null;
+  private id: number | null = null;
   protected balanceString: string = '0 Ft';
   protected balance: number = 0;
 
+  constructor (private userService: UserService) {}
+
   ngOnInit(): void {
     this.username = this.getUsername();
+    this.id = this.getId();
+
+    if(this.id){
+      this.getBalance();
+    }
   }
 
   getUsername(): string | null {
@@ -19,10 +30,30 @@ export class HeaderComponent implements OnInit{
     return userData ? userData : null;
   }
 
-  deleteUser(): void {
-    localStorage.removeItem('name');
-    localStorage.removeItem('id');
-    localStorage.removeItem('token');
-    this.username = null;
+  getId(): number | null {
+    const userData = localStorage.getItem('id');
+    return userData ? Number(userData) : null;
+  }
+
+  getBalance(): void {
+    this.subscription.add(
+      this.userService.getBalance(this.id ? this.id : 0).subscribe(
+        (balance: number) => {
+          this.balance = balance;
+          this.updateBalance();
+        },
+        (error) => {
+          console.error('Error fetching balance:', error);
+        }
+      )
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  updateBalance(): void{
+    this.balanceString = this.balance + ' Ft';
   }
 }
