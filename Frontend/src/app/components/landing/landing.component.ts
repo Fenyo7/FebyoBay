28069c57676c038015e73a53cc7d2c9617da0a03
Fenyo7 <fenyo.lev@gmail.com>
2023-10-21 20:38:18 +0200

    feat(fe): landing page rework, rename, new icon

--- Frontend/src/app/components/landing/landing.component.ts
+++ Frontend/src/app/components/landing/landing.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.css']
+  styleUrls: ['./landing.component.css'],
 })
 export class LandingComponent {
-
+  // Features array
+  features: string[] = [
+    'Register to the application',
+    'Sign in to the application',
+    'Modify user data',
+    'Delete user account',
+    'Logout of the application',
+    'Create a sellable item',
+    'List existing sellable items',
+    "View a specific sellable item's details",
+    'Buy a sellable item',
+    "Edit a user's item",
+    "Delete a user's item",
+    'Fortune wheel to increase wealth',
+    'Backend integration tests',
+    'Backend unit tests',
+    'Frontend snapshot tests',
+    'Frontend & Backend CI',
+    'Deployed to Railway through CD',
+  ];
 }
--- Frontend/src/app/material/material.module.ts
+++ Frontend/src/app/material/material.module.ts
@@ -2,21 +2,21 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatMenuModule} from '@angular/material/menu';
- 
+import { MatMenuModule } from '@angular/material/menu';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+
 const MaterialComponents = [
   MatButtonModule,
   MatIconModule,
   MatToolbarModule,
   MatMenuModule,
-]
+  MatCardModule,
+  MatListModule,
+];
 
 @NgModule({
-  imports: [
-    MaterialComponents
-  ],
-  exports: [
-    MaterialComponents
-  ]
+  imports: [MaterialComponents],
+  exports: [MaterialComponents],
 })
-export class MaterialModule { }
+export class MaterialModule {}
